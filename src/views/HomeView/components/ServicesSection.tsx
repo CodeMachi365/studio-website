c970249c0e144bac831441be7885e4146eac6489
yht <121574470+code-tiger@users.noreply.github.com>
2025-02-26 22:30:03 +0800

    feat: services section dark mode

--- src/views/HomeView/components/ServicesSection.tsx
+++ src/views/HomeView/components/ServicesSection.tsx
@@ -13,11 +13,11 @@ export default function ServicesSection() {
   return (
     <section id={SERVICES_SECTION_ID} className="home-view-section py-8">
       <h2 className="text-3xl font-bold text-center mb-4">Services</h2>
-      <span className="text-xl text-center block mb-8">What I offer</span>
+      <span className="text-xl text-center block mb-8">What We Offer</span>
       <div className="services__container container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         <ServiceContent
           icon={<FaMobileAlt className="text-4xl text-blue-500" />}
-          title="Mobile Application"
+          title="Mobile Applications"
           description="Building efficient apps for multiple platforms using Swift, React Native, Flutter, or other cross-platform frameworks."
         />
         <ServiceContent
@@ -55,12 +55,11 @@ function ServiceContent({ icon, title, description }) {
     <div className="services__content p-4 border rounded-lg shadow-lg">
       <div className="icon__box flex items-center justify-center mb-4">
         {icon}
-        <div className="services__dot ml-2">
-          <span className="dot w-2 h-2 bg-gray-500 rounded-full inline-block"></span>
-        </div>
       </div>
       <h3 className="services__title text-xl font-semibold mb-2">{title}</h3>
-      <p className="services__description text-gray-700">{description}</p>
+      <p className="services__description text-secondary dark:text-secondary-dark">
+        {description}
+      </p>
     </div>
   );
 }
